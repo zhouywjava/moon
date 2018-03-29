b39e9ca40fff2f8953e20708f4b6d7bc9cb0f32e
zhouyiwei <67976661@qq.com>
2018-03-29 23:03:19 +0800

    Initial commit

---
+++ src/main/java/com/zyw/moon/MoonApplication.java
@@ -0,0 +1,12 @@
+package com.zyw.moon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zyw/moon/MoonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zyw.moon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MoonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
